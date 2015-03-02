75a08eafc72036fd27e051e74dc2cd2548c4b269
ZoOL <zhooul@gmail.com>
2015-03-02 14:00:02 +0800

    rspec3 style

--- spec/apicop/oauth2/access_token_spec.rb
+++ spec/apicop/oauth2/access_token_spec.rb
@@ -11,18 +11,25 @@ describe APICop::OAuth2::AccessToken do
   end
   subject { token }
 
-  its(:access_token)  { should == 'access_token' }
-  its(:refresh_token) { should == 'refresh_token' }
-  its(:expires_in)    { should == 3600 }
-  its(:scope)         { should == [:scope1, :scope2] }
-  its(:token_response) do
-    should == {
-      :token_type => :bearer,
-      :access_token => 'access_token',
-      :refresh_token => 'refresh_token',
-      :expires_in => 3600,
-      :scope => 'scope1 scope2'
-    }
+  context '#access_token' do
+    it { expect(subject.access_token).to eq 'access_token' }
+  end
+  context '#refresh_token' do
+    it { expect(subject.refresh_token).to eq 'refresh_token' }
+  end
+  context '#expires_in' do
+    it { expect(subject.expires_in).to eq 3600 }
+  end
+  context '#scope' do
+    it { expect(subject.scope).to eq [:scope1, :scope2] }
+  end
+  context '#token_response' do
+    it { expect(subject.token_response).to eq({ :token_type => :bearer,
+                                                :access_token => 'access_token',
+                                                :refresh_token => 'refresh_token',
+                                                :expires_in => 3600,
+                                                :scope => 'scope1 scope2'
+                                              }) }
   end
 
   context 'when access_token is missing' do
@@ -52,7 +59,7 @@ describe APICop::OAuth2::AccessToken do
     describe method do
       it 'should delegate to HTTPClient with Authenticator filter' do
         expect(token.httpclient).to receive(method).with(resource_endpoint)
-        token.httpclient.request_filter.last.should be_a APICop::OAuth2::AccessToken::Authenticator
+        expect(token.httpclient.request_filter.last).to be_a APICop::OAuth2::AccessToken::Authenticator
         token.send method, resource_endpoint
       end
     end
@@ -61,7 +68,7 @@ describe APICop::OAuth2::AccessToken do
       it do
         APICop::OAuth2.debug do
           token.httpclient.request_filter[-2].should be_a APICop::OAuth2::AccessToken::Authenticator
-          token.httpclient.request_filter.last.should be_a APICop::OAuth2::Debugger::RequestFilter
+          expect(token.httpclient.request_filter[-2]).to be_a APICop::OAuth2::AccessToken::Authenticator
         end
       end
     end
