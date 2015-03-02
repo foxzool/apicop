b40810599305a857e240418054e144dbcbdddab5
ZoOL <zhooul@gmail.com>
2015-03-02 11:55:33 +0800

    rspec3 style

--- spec/apicop/oauth2/oauth2_spec.rb
+++ spec/apicop/oauth2/oauth2_spec.rb
@@ -4,28 +4,35 @@ describe APICop::OAuth2 do
   subject { APICop::OAuth2 }
   after { APICop::OAuth2.debugging = false }
 
-  its(:logger) { should be_a Logger }
-  its(:debugging?) { should == false }
+  context 'logger' do
+    it { expect(subject.logger).to be_a(Logger) }
+  end
+
+  context 'debugging?' do
+    it { expect(subject.debugging?).to be_falsey }
+  end
 
   describe '.debug!' do
     before { APICop::OAuth2.debug! }
-    its(:debugging?) { should == true }
+    context 'debugging?' do
+      it { expect(subject.debugging?).to be_truthy }
+    end
   end
 
   describe '.debug' do
     it 'should enable debugging within given block' do
       APICop::OAuth2.debug do
-        APICop::OAuth2.debugging?.should == true
+        expect(APICop::OAuth2.debugging?).to be_truthy
       end
-      APICop::OAuth2.debugging?.should == false
+      expect(APICop::OAuth2.debugging?).to be_falsey
     end
 
     it 'should not force disable debugging' do
       APICop::OAuth2.debug!
       APICop::OAuth2.debug do
-        APICop::OAuth2.debugging?.should == true
+        expect(APICop::OAuth2.debugging?).to be_truthy
       end
-      APICop::OAuth2.debugging?.should == true
+      expect(APICop::OAuth2.debugging?).to be_truthy
     end
   end
 
@@ -39,7 +46,7 @@ describe APICop::OAuth2 do
           APICop::OAuth2.http_config do |config|
             config.request_filter << Proc.new {}
           end
-          APICop::OAuth2.http_client.request_filter.last.should be_instance_of APICop::OAuth2::Debugger::RequestFilter
+          expect(APICop::OAuth2.http_client.request_filter.last).to be_instance_of APICop::OAuth2::Debugger::RequestFilter
         end
 
         it 'should reset_http_config' do
@@ -49,7 +56,7 @@ describe APICop::OAuth2 do
           end
           size = APICop::OAuth2.http_client.request_filter.size
           APICop::OAuth2.reset_http_config!
-          APICop::OAuth2.http_client.request_filter.size.should == size - 1
+          expect(APICop::OAuth2.http_client.request_filter.size).to eq(size - 1)
         end
 
       end
@@ -59,15 +66,14 @@ describe APICop::OAuth2 do
   describe ".http_client" do
     context "when local_http_config is used" do
       it "should correctly set request_filter" do
-        clnt1 = APICop::OAuth2.http_client
-        clnt2 = APICop::OAuth2.http_client("my client") do |config|
+        client1 = APICop::OAuth2.http_client
+        client2 = APICop::OAuth2.http_client("my client") do |config|
           config.request_filter << Proc.new {}
         end
-        clnt3 = APICop::OAuth2.http_client
+        client3 = APICop::OAuth2.http_client
 
-        clnt1.request_filter.size.should == clnt3.request_filter.size
-        clnt1.request_filter.size.should == clnt2.request_filter.size - 1
-
+        expect(client1.request_filter.size).to eq(client3.request_filter.size)
+        expect(client1.request_filter.size).to eq(client2.request_filter.size - 1)
       end
     end
   end
